d1659689b536b128687987dd46ee4ee8eebd26dc
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2025-06-27 21:37:40 +0300

    feat: strapi query builder

--- store/features/mainPageApi.ts
+++ store/features/mainPageApi.ts
@@ -1,17 +1,90 @@
 import { StrapiMainPageResponse } from '@/types/strapi';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+export const buildStrapiQuery = (
+  endpoint: string,
+  options: {
+    populate?: string[] | '*' | 'deep';
+    fields?: string[];
+    filters?: Record<string, never>;
+    sort?: string[];
+    pagination?: { page?: number; pageSize?: number };
+  }
+) => {
+  const params = new URLSearchParams();
+
+  if (options.populate) {
+    if (typeof options.populate === 'string') {
+      params.append('populate', options.populate);
+    } else {
+      // Use indexed notation for multiple populate parameters
+      options.populate.forEach((field, index) => {
+        params.append(`populate[${index}]`, field);
+      });
+    }
+  }
+
+  if (options.fields) {
+    params.append('fields', options.fields.join(','));
+  }
+
+  if (options.filters) {
+    Object.entries(options.filters).forEach(([key, value]) => {
+      params.append(`filters[${key}]`, String(value));
+    });
+  }
+
+  if (options.sort) {
+    options.sort.forEach((field) => params.append('sort', field));
+  }
+
+  if (options.pagination) {
+    if (options.pagination.page) {
+      params.append('pagination[page]', options.pagination.page.toString());
+    }
+    if (options.pagination.pageSize) {
+      params.append(
+        'pagination[pageSize]',
+        options.pagination.pageSize.toString()
+      );
+    }
+  }
+
+  return `${endpoint}?${params.toString()}`;
+};
+
 export const mainPageApi = createApi({
-  reducerPath: 'mainPageApi',
+  reducerPath: 'mainPageApiWithHelper',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://technology-strapi.onrender.com/api/',
+    baseUrl: process.env.NEXT_PUBLIC_STRAPI_HOST,
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
     },
   }),
   endpoints: (builder) => ({
     getMainPage: builder.query<StrapiMainPageResponse, void>({
-      query: () => 'main-page?populate=*',
+      query: () => {
+        console.log(
+          buildStrapiQuery('main-page', {
+            populate: [
+              'hero',
+              'aboutUs.images',
+              'whatWeDo.images',
+              'whyChooseUs.images',
+              'closing.images',
+            ],
+          })
+        );
+        return buildStrapiQuery('main-page', {
+          populate: [
+            'hero',
+            'aboutUs.images',
+            'whatWeDo.images',
+            'whyChooseUs.images',
+            'closing.images',
+          ],
+        });
+      },
     }),
   }),
 });
