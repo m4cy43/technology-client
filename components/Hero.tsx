4f02fce3532c081850022ac428d39445fbcd18b3
VIO <75546139+violettpv@users.noreply.github.com>
2025-06-30 21:47:21 +0300

    feat: hero section css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="flex h-full flex-col p-10">
-      <div className="h-60 w-[70%] pt-4 pb-4 pl-6 lg:pt-7">
+    <div className="flex h-full flex-col p-4 md:p-10">
+      <div className="hero-title-block h-60 w-[60%] pt-4 pb-4 pl-6 lg:pt-7">
         <h1 className="main-title text-5xl leading-[1.5em] font-semibold">
           Your Partner in Construction, Engineering & Business Growth
         </h1>
       </div>
-      <div className="flex h-[200px] flex-col rounded-3xl bg-(--hero-bg) px-5 py-6 md:h-[300px] md:flex-row lg:h-[400px]">
+      <div className="hero-media-block flex h-[400px] flex-col rounded-3xl bg-(--hero-bg) px-5 py-6 md:flex-row">
         <div className="w-[50%]">
           <p className="text-xl leading-[1.6em]">
             At Technology, we combine manufacturing excellence, innovative
@@ -19,7 +19,7 @@ const Hero = () => {
             every project.
           </p>
         </div>
-        <div className="relative flex w-[50%] items-center justify-end">
+        <div className="hero-image-wrapper relative flex w-[50%] items-start justify-center">
           <Image
             id="hero-image"
             src="/сollab-bro.svg"
